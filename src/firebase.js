a44526235f8c2b622b5fe3c1bdcccdc9145cb71e
squamuglia <maxsmouha@gmail.com>
2019-03-04 12:45:59 -0500

    firestore

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+require("firebase/firestore");
+
+firebase.initializeApp({
+	apiKey: "AIzaSyCyqVx31g5-GDoUmoMEPLHKcqmid5UrJIE",
+	authDomain: "wework-beer.firebaseapp.com",
+	databaseURL: "https://wework-beer.firebaseio.com",
+	projectId: "wework-beer",
+	storageBucket: "wework-beer.appspot.com",
+	messagingSenderId: "995289530050"
+});
+
+firebase.firestore();
+
+export default firebase;
